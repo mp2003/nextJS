1e61b0361a5fb5f26daac98618e25132402d9210
mahi5256 <m.p6092003@gmail.com>
2023-10-08 21:25:34 +0530

    next js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-
-const index = () => {
+export default function Home() {
   return (
-    <div>index</div>
-  )
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Currently pursuing B.Tech in Computer Science and Engineering at BTKIT Dwarahat.
+        Possess a natural inclination towards finding innovative solutions to problems, which has driven me to pursue a career in computer science.
+        Have been exploring and experimenting with technology since my high school years.</p>
+        <p> connect with me on <Link href='https://www.linkedin.com/in/milind-pandey0609/'> Linkdin</Link></p>
+        
+      </section>
+    </Layout>
+  );
 }
-
-export default index
