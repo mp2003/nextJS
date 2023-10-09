c1c90446010baf41917c2abcdefcff1cee794273
mahi5256 <m.p6092003@gmail.com>
2023-10-09 23:15:28 +0530

    dynamic routing

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,6 +1,25 @@
 import Layout from '../../components/layout';
-
 import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
 export async function getStaticProps({ params }) {
   const postData = getPostData(params.id);
@@ -10,21 +29,3 @@ export async function getStaticProps({ params }) {
     },
   };
 }
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
-}
-export default function Post({ postData }) {
-    return (
-      <Layout>
-        {postData.title}
-        <br />
-        {postData.id}
-        <br />
-        {postData.date}
-      </Layout>
-    );
-  }
