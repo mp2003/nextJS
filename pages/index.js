4ca04555d66602f2143ba88ada3935deda94ccb7
mahi5256 <m.p6092003@gmail.com>
2023-10-09 22:00:07 +0530

    fetchimg data

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,16 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
 
 export default function Home() {
   return (
